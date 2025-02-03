7465948e0441c313ec38ca16f0b31cc4c99576e9
Gabriel.ortizz <gabrielmd0graus@gmail.com>
2025-02-03 13:17:12 -0300

    refactoring:home

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import RoomList from '../components/RoomList';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
@@ -10,6 +11,11 @@ const Home: React.FC = () => {
       <main className="container mx-auto p-4">
         <h2 className="text-2xl font-bold mb-4">Escolha seu quarto</h2>
         <RoomList />
+        <div className="mt-4">
+          <Link to="/login" className="text-blue-600 hover:underline">
+            Fazer login
+          </Link>
+        </div>
       </main>
       <Footer />
     </div>
